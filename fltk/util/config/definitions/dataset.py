eb4c15c5714bb02166f854a95a0874ebb9e9b613
Ramazan <ramazanttan@gmail.com>
2026-01-26 11:53:31 +0100

    paper_method

---
+++ fltk/util/config/definitions/dataset.py
@@ -0,0 +1,18 @@
+from aenum import unique, Enum
+from typing import TypeVar
+
+T = TypeVar('T')
+
+
+@unique
+class Dataset(Enum):
+    cifar10 = 'cifar10'
+    cifar100 = 'cifar100'
+    fashion_mnist = 'fashion-mnist'
+    mnist = 'mnist'
+
+    @classmethod
+    def _missing_name_(cls, name: str) -> T:
+        for member in cls:
+            if member.name.lower() == name.lower():
+                return member
